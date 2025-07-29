fc040b9586a6c91d6f15c68194f7665cb64f6487
derussell23 <djamesr23@gmail.com>
2025-07-28 20:05:17 -0400

    chore: remove obsolete build files and dependencies from physics plugin

--- backend/aerodynamics/physics_plugin/build 12/_deps/cmocka-build/example/CMakeFiles/assert_module_test.dir/assert_module.c.o.d
+++
@@ -1,9 +0,0 @@
-_deps/cmocka-build/example/CMakeFiles/assert_module_test.dir/assert_module.c.o: \
-  /Users/derekjrussell/Documents/repos/IronMan/backend/aerodynamics/physics_plugin/build/_deps/cmocka-src/example/assert_module.c \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/assert.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/cdefs.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_symbol_aliasing.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/sys/_posix_availability.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/_assert.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX15.2.sdk/usr/include/_static_assert.h \
-  /Users/derekjrussell/Documents/repos/IronMan/backend/aerodynamics/physics_plugin/build/_deps/cmocka-src/example/assert_module.h
